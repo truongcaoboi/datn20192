0ad5d517ace2cfe0afda5b485c8710c9b4663a9c
truongcaoboi <tentromvodanh@gmail.com>
2020-03-16 18:02:35 +0700

    create project

---
+++ e-learning/src/main/java/net/bk/datn/elearning/ELearningApplication.java
@@ -0,0 +1,13 @@
+package net.bk.datn.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningApplication.class, args);
+	}
+
+}
---
+++ e-learning/src/test/java/net/bk/datn/elearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bk.datn.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
